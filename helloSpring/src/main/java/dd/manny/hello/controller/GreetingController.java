0cb9ea6b50eb480024f5dc702ee24fc8ccbca14b
Emanuele Galli <egalli64@gmail.com>
2016-05-30 22:45:41 +0200

    http://bitingcode.blogspot.com/2016/05/spring-log-only-to-file.html

--- helloSpring/src/main/java/dd/manny/hello/controller/GreetingController.java
+++ helloSpring/src/main/java/dd/manny/hello/controller/GreetingController.java
@@ -1,12 +1,22 @@
 package dd.manny.hello.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+    private static final Log log = LogFactory.getLog(GreetingController.class);
+
     @RequestMapping("/greeting")
     public String greeting() {
+        log.trace("trace hello");
+        log.debug("debug hello");
+        log.info("info hello");
+        log.warn("warn hello");
+        log.error("error hello");
+        log.fatal("fatal hello");
         return "Hello!";
     }
 }
