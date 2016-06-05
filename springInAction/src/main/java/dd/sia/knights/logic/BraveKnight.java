eeb347d5c915685b9f8185f1cb1127a53e7169e7
Emanuele Galli <egalli64@gmail.com>
2016-06-05 23:24:35 +0200

    Autowired constructor

--- springInAction/src/main/java/dd/sia/knights/logic/BraveKnight.java
+++ springInAction/src/main/java/dd/sia/knights/logic/BraveKnight.java
@@ -1,13 +1,19 @@
 package dd.sia.knights.logic;
-  
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("knight")
 public class BraveKnight implements Knight {
-  private Quest quest;
+    private Quest quest;
 
-  public BraveKnight(Quest quest) {
-    this.quest = quest;
-  }
+    @Autowired
+    public BraveKnight(@Qualifier("quest") Quest quest) {
+        this.quest = quest;
+    }
 
-  public String doQuest() {
-    return quest.embark();
-  }
+    public String doQuest() {
+        return quest.embark();
+    }
 }
---
+++ springInAction/src/main/java/dd/sia/knights/logic/SlayDragonQuest.java
@@ -0,0 +1,12 @@
+package dd.sia.knights.logic;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("quest")
+public class SlayDragonQuest implements Quest {
+    public String embark() {
+        return "Embarking on quest to slay the dragon!";
+    }
+}
